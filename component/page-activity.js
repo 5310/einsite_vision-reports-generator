3fc6836c67641827dd920825b118381e86a37d05
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2021-03-19 17:24:15 +0530

    Test implementing equipment object

--- component/page-activity.js
+++ component/page-activity.js
@@ -3,18 +3,23 @@ import { LitElement, html, css } from 'https://cdn.skypack.dev/lit-element'
 export default class VisionReportPageActivity extends LitElement {
   static get properties() {
     return {
+      equipment: { type: Object },
       reportName: { type: String, attribute: 'report-name' },
       date: { type: Object },
-      pageTitle: { type: String, attribute: 'page-title' },
       pageNo: { type: Number, attribute: 'page-no' },
     }
   }
 
   constructor() {
     super()
+    this.equipment = {
+      index: 0,
+      id: 'CRL 15 - 988K',
+      type: 'Front Loader',
+    }
     this.reportName = 'Report Template'
     this.date = 'Weekday, Mon D, Year'
-    this.pageTitle = 'Title'
+    this.pageTitle = `Activity Map – ${this.equipment.id}`
     this.pageNo = '#'
   }
 
@@ -68,7 +73,7 @@ export default class VisionReportPageActivity extends LitElement {
       <vision-report-page
         .reportName=${this.reportName}
         .date=${this.date}
-        .pageTitle=${this.pageTitle}
+        .pageTitle=${`Activity Map – ${this.equipment.id}`}
         .pageNo=${this.pageNo}
       >
         <vision-report-map class="map"></vision-report-map>
